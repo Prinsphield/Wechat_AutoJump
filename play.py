d440712af7fc7bfa084bb7d108a68f66aa64b57f
xiaoth <xiaoth@mbp.local>
2018-01-01 00:29:28 +0800

    renew

--- play.py
+++ play.py
@@ -11,12 +11,12 @@ from multiprocessing import Pool
 from functools import partial
 from itertools import repeat
 
-def multi_scale_search(pivot, screen):
+def multi_scale_search(pivot, screen, range=0.3, num=10):
     H, W = screen.shape[:2]
     h, w = pivot.shape[:2]
 
     found = None
-    for scale in np.linspace(0.7, 1.3, 10)[::-1]:
+    for scale in np.linspace(1-range, 1+range, num)[::-1]:
         resized = cv2.resize(screen, (int(W * scale), int(H * scale)))
         r = W / float(resized.shape[1])
         if resized.shape[0] < h or resized.shape[1] < w:
@@ -36,7 +36,8 @@ def multi_scale_search(pivot, screen):
     return [start_h, start_w, end_h, end_w, score]
 
 class WechatAutoJump(object):
-    def __init__(self, resolution, sensitivity, debug, resource_dir):
+    def __init__(self, phone, resolution, sensitivity, debug, resource_dir):
+        self.phone = phone
         self.resolution = resolution
         self.scale = self.resolution[1]/720.
         self.sensitivity = sensitivity
@@ -44,6 +45,9 @@ class WechatAutoJump(object):
         self.resource_dir = resource_dir
         self.step = 0
         self.load_resource()
+        if self.phone == 'IOS':
+            self.client = wda.Client()
+            self.sess = self.client.session()
 
     def load_resource(self):
         self.player = cv2.imread(os.path.join(self.resource_dir, 'player.png'), 0)
@@ -52,66 +56,77 @@ class WechatAutoJump(object):
         self.jump_file = [cv2.imread(name, 0) for name in circle_file + table_file]
 
     def get_current_state(self):
-        os.system('adb shell screencap -p /sdcard/1.png')
-        os.system('adb pull /sdcard/1.png state.png')
+        if self.phone == 'Android':
+            os.system('adb shell screencap -p /sdcard/1.png')
+            os.system('adb pull /sdcard/1.png state.png')
+        elif self.phone == 'IOS':
+            self.client.screenshot('state.png')
+
         if self.debug:
             shutil.copyfile('state.png', 'state_{:03d}.png'.format(self.step))
 
-        state = cv2.imread('state.png', 0)
+        state = cv2.imread('state.png')
         state = cv2.resize(state, (720, int(self.resolution[0] / self.scale)))
         return state
 
     def get_player_position(self, state):
-        return multi_scale_search(self.player, state)
-
-    def get_target_position_fast(self, state, player_pos):
-        state_cut = state[:player_pos[2],:].copy()
-        jump_file = self.jump_file.copy()
-        pool = Pool(5)
-        partial_search = partial(multi_scale_search, screen=state_cut)
-        positions = pool.map(partial_search, jump_file)
-        # positions = pool.starmap(multi_scale_search, zip(jump_file, repeat(state_cut)))
-        pool.close()
-        pool.join()
-        max_ind = np.argmax(np.array(positions)[:,-1])
-        target_pos = positions[max_ind]
-        return target_pos
+        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+        pos = multi_scale_search(self.player, state, 0.3, 10)
+        h, w = int((pos[0] + 13 * pos[2])/14.), (pos[1] + pos[3])//2
+        return np.array([h, w])
 
     def get_target_position(self, state, player_pos):
-        state_cut = state[:player_pos[2],:]
+        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+        state_cut = state[:player_pos[0],:]
         target_pos = None
         for target in self.jump_file:
-            pos = multi_scale_search(target, state_cut)
+            pos = multi_scale_search(target, state_cut, 0.4, 15)
             if target_pos is None or pos[-1] > target_pos[-1]:
                 target_pos = pos
-        return target_pos
+        return np.array([(target_pos[0]+target_pos[2])//2, (target_pos[1]+target_pos[3])//2])
+
+    def get_target_position_fast(self, state, player_pos):
+        state_cut = state[:player_pos[0],:,:]
+        a = np.uint8((state_cut == [245,245,245]).astype(np.float32)[:, :, 0] * 255)
+        b1, b2 = cv2.connectedComponents(a)
+        for i in range(1, np.max(b2) + 1):
+            x, y = np.where(b2 == i)
+            # print('fast', len(x))
+            if len(x) > 280 and len(x) < 310:
+                r_x, r_y = x, y
+        h, w = int(r_x.mean()), int(r_y.mean())
+        return np.array([h, w])
 
     def jump(self, player_pos, target_pos):
-        p_s = np.array([player_pos[2], (player_pos[1]+player_pos[3])//2])
-        p_e = np.array([(target_pos[0]+target_pos[2])//2, (target_pos[1]+target_pos[3])//2])
-        distance = np.linalg.norm(p_s - p_e)
-
+        distance = np.linalg.norm(player_pos - target_pos)
         press_time = distance * self.sensitivity
         press_time = int(press_time)
-        cmd = 'adb shell input swipe 320 410 320 410 ' + str(press_time)
-        print(cmd)
-        os.system(cmd)
+        if self.phone == 'Android':
+            press_h, press_w = int(0.82*self.resolution[0]), self.resolution[1]//2
+            cmd = 'adb shell input swipe {} {} {} {} {}'.format(press_w, press_h, press_w, press_h, press_time)
+            print(cmd)
+            os.system(cmd)
+        elif self.phone == 'IOS':
+            self.sess.tap_hold(200, 200, press_time / 1000.)
 
     def debugging(self):
         current_state = self.state.copy()
-        cv2.rectangle(current_state, (self.player_pos[1], self.player_pos[0]), (self.player_pos[3], self.player_pos[2]), (0,255,0), 2)
-        cv2.rectangle(current_state, (self.target_pos[1], self.target_pos[0]), (self.target_pos[3], self.target_pos[2]), (0,0,255), 2)
-        cv2.imwrite('state_{:03d}_res.png'.format(self.step), current_state)
+        cv2.circle(current_state, (self.player_pos[1], self.player_pos[0]), 5, (0,255,0), -1)
+        cv2.circle(current_state, (self.target_pos[1], self.target_pos[0]), 5, (0,0,255), -1)
+        cv2.imwrite('state_{:03d}_res_h_{}_w_{}.png'.format(self.step, self.target_pos[0], self.target_pos[1]), current_state)
 
     def play(self):
         self.state = self.get_current_state()
         self.player_pos = self.get_player_position(self.state)
-        self.target_pos = self.get_target_position(self.state, self.player_pos)
+        try:
+            self.target_pos = self.get_target_position_fast(self.state, self.player_pos)
+        except:
+            self.target_pos = self.get_target_position(self.state, self.player_pos)
         if self.debug:
             self.debugging()
         self.jump(self.player_pos, self.target_pos)
         self.step += 1
-        time.sleep(1)
+        time.sleep(1.5)
 
     def run(self):
         try:
@@ -120,24 +135,16 @@ class WechatAutoJump(object):
         except KeyboardInterrupt:
                 pass
 
-    def test_detection(self, file):
-        self.state = cv2.imread(file, 0)
-        self.player_pos = self.get_player_position(self.state)
-        self.target_pos = self.get_target_position(self.state, self.player_pos)
-        current_state = self.state.copy()
-        cv2.rectangle(current_state, (self.player_pos[1], self.player_pos[0]), (self.player_pos[3], self.player_pos[2]), (0,255,0), 2)
-        cv2.rectangle(current_state, (self.target_pos[1], self.target_pos[0]), (self.target_pos[3], self.target_pos[2]), (0,0,255), 2)
-        cv2.imwrite('state.png', current_state)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--phone', default='Android', choices=['Android', 'IOS'], type=str, help='OS')
     parser.add_argument('--resolution', default=[1280, 720], nargs=2, type=int, help='mobile phone resolution')
-    parser.add_argument('--sensitivity', default=2.05, type=float)
+    parser.add_argument('--sensitivity', default=2.051, type=float)
     parser.add_argument('--resource', default='resource', type=str)
     parser.add_argument('--debug', default=False, action='store_true')
     args = parser.parse_args()
     # print(args)
 
-    AI = WechatAutoJump(args.resolution, args.sensitivity, args.debug, args.resource)
+    AI = WechatAutoJump(args.phone, args.resolution, args.sensitivity, args.debug, args.resource)
     AI.run()
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # 自动玩微信小游戏跳一跳
 
-### Requirement
+### Requirements
 
 - Python
 - Opencv
@@ -11,12 +11,18 @@
 
 	python play2.py --phone Android --resolution 1280 720 --sensitivity 2.051
 
-`--phone` has two options: Android or IOS.
-`--resolution` is your phone's height and width.
-`--sensitivity` is the constant parameter that controls the pressing time.
+- `--phone` has two options: Android or IOS.
+- `--resolution` is your phone's height and width.
+- `--sensitivity` is the constant parameter that controls the pressing time.
+
+It is easy to reach the state of art as long as you like.
+![screen shot](resource/screen_shot.png)
+<div align="center">
+<img align="center" src="resource/screen_shot.png" width="450" alt="cross">
+</div> 
+<br/>
 
 Here is a video demo. (old version)
-
 Current version is much faster than this one. Excited!
 
 [![微信跳一跳](https://img.youtube.com/vi/MQ0SCnOcjaI/0.jpg)](https://youtu.be/MQ0SCnOcjaI "自动玩微信小游戏跳一跳")
